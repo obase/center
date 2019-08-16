f3fd22e92f16be6cf348d844e3466f09975a15a7
hezhaowu <hezhaowu@kingsoft.com>
2019-08-16 11:56:00 +0800

    剔除828兼容逻辑,方便后续统一扩展逻辑

--- centerinit.go
+++ centerinit.go
@@ -5,33 +5,25 @@ import (
 )
 
 const (
-	PCKEY1 = "service.center"
-	PCKEY2 = "ext.service.centerAddr"
-
-	OFF = "off"
+	CKEY  = "service.center"
+	LOCAL = "local"
 )
 
+/*
+不再兼容828旧逻辑,没有"service.center"就不会自动启用!
+*/
 func init() {
-	config, ok := conf.Get(PCKEY1)
+	config, ok := conf.Get(CKEY)
 	if !ok {
-		// 828要求server.runmode必须为DEBUG才会启用centerAddr
-		if conf.OptiString("server.runmode", "release") == "debug" {
-			config, ok = conf.Get(PCKEY2)
-		}
+		return
 	}
-	/*
-	   为了兼容旧的828逻辑, 在没有声明center或centerAddr的情况默认连接本地. 所以声明一个特殊值"none","off"表示关闭
-	*/
 	switch config := config.(type) {
 	case nil:
-		Setup(&Config{Address: ""})
+		Setup(&Config{Address: LOCAL})
 	case string:
-		if config != OFF {
-			Setup(&Config{Address: config})
-		}
+		Setup(&Config{Address: config})
 	case map[interface{}]interface{}:
 		var service map[string][]string
-
 		address, ok := conf.ElemString(config, "address")
 		expired, ok := conf.ElemInt64(config, "expired")
 		tmp, ok := conf.ElemMap(config, "service")
