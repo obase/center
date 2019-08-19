edd8cd75af521ebcd58e2b6922242d00dbf6640e
hezhaowu <hezhaowu@kingsoft.com>
2019-08-19 10:18:57 +0800

    save

--- consul.go
+++ consul.go
@@ -10,7 +10,7 @@ import (
 const DEFAULT_EXPIRED int64 = 5 //与828 center默认值相同
 
 type consulEntry struct {
-	Mtime   int64      // 最后修改时间戳
+	sync.RWMutex
 	Index   uint64     // 最后刷新的下标
 	Service []*Service // 服务项
 }
@@ -27,7 +27,7 @@ func newConsulClient(opt *Config) Center {
 	var err error
 
 	config := api.DefaultConfig()
-	if opt.Address != LOCAL {
+	if opt.Address != "" {
 		config.Address = opt.Address
 	}
 	if client, err = api.NewClient(config); err != nil { // 兼容旧的逻辑
@@ -38,10 +38,63 @@ func newConsulClient(opt *Config) Center {
 		}
 	}
 
-	return &consulClient{
+	ret := &consulClient{
 		Client:  client,
 		Entries: make(map[string]*consulEntry),
 		Expired: nvl(opt.Expired, DEFAULT_EXPIRED),
+	}
+	// 启动刷新后台,定期更新entires的数据
+	go refreshConsulClientEntries(ret)
+
+	return ret
+}
+
+const maxprocs = 8 // 最多起8个协程处理后台更新
+func refreshConsulClientEntries(c *consulClient) {
+	for _ = range time.Tick(time.Duration(c.Expired) * time.Second) {
+		if len(c.Entries) <= maxprocs {
+			// 数量不超maxprocs不需分组
+			wg := new(sync.WaitGroup)
+			c.RWMutex.RLock()
+			for name, entry := range c.Entries {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					c.refresh(name, entry)
+				}()
+			}
+			c.RWMutex.RUnlock()
+			wg.Wait()
+		} else {
+			// 数量超过maxprocs需要分组
+			set := make([]map[string]*consulEntry, maxprocs)
+			cnt := 0
+			c.RWMutex.RLock()
+			for name, entry := range c.Entries {
+				idx := cnt % maxprocs
+				if set[idx] == nil {
+					set[idx] = make(map[string]*consulEntry)
+				}
+				set[idx][name] = entry
+				cnt++
+			}
+			c.RWMutex.RUnlock()
+
+			wg := new(sync.WaitGroup)
+			for _, part := range set {
+				if len(part) > 0 {
+					wg.Add(1)
+					go func() {
+						defer wg.Done()
+						for name, entry := range part {
+							c.refresh(name, entry)
+						}
+					}()
+				}
+			}
+			wg.Wait()
+		}
+
 	}
 }
 
@@ -90,31 +143,48 @@ func (c *consulClient) Deregister(serviceId string) (err error) {
 	return c.Agent().ServiceDeregister(serviceId)
 }
 
-func (c *consulClient) FetchService(name string) ([]*Service, uint64, error) {
+func (c *consulClient) FetchService(name string) (service []*Service, index uint64, err error) {
 	var (
 		entry *consulEntry
 		ok    bool
-		err   error
-		now   = time.Now().Unix()
 	)
 	c.RWMutex.RLock()
 	entry, ok = c.Entries[name]
 	c.RWMutex.RUnlock()
-	if !ok || now-entry.Mtime > c.Expired {
+	if !ok {
 		c.RWMutex.Lock()
 		entry, ok = c.Entries[name]
-		if !ok || now-entry.Mtime > c.Expired { // 二次检测
+		if !ok { // 二重检测
 			entry = new(consulEntry)
-			entry.Mtime = now
-			entry.Service, entry.Index, err = c.WatchService(name, entry.Index)
-			if err == nil {
-				c.Entries[name] = entry
-			}
+			c.Entries[name] = entry
 		}
 		c.RWMutex.Unlock()
 	}
-	return entry.Service, entry.Index, err
+	entry.RWMutex.RLock()
+	service, index = entry.Service, entry.Index
+	entry.RWMutex.RUnlock()
+	if service == nil {
+		entry.RWMutex.Lock()
+		service, index = entry.Service, entry.Index
+		if service == nil { // 二重检测
+			service, index, err = c.refresh(name, entry)
+		}
+		entry.RWMutex.Unlock()
+	}
+	return
 }
+
+func (c *consulClient) refresh(name string, entry *consulEntry) (service []*Service, index uint64, err error) {
+	if service, index, err = c.WatchService(name, 0); err == nil {
+		if index != entry.Index {
+			entry.RWMutex.Lock()
+			entry.Service, entry.Index = service, index
+			entry.RWMutex.Unlock()
+		}
+	}
+	return
+}
+
 func (c *consulClient) WatchService(name string, index uint64) ([]*Service, uint64, error) {
 	var options *api.QueryOptions
 	if index > 0 {
